d148d129df738440ba837b151fe0d297e942ab8c
fbara <fbara1@yahoo.com>
2021-08-13 09:47:44 -0500

    Initial Commit

---
+++ Top Headlines/Top_HeadlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Top_HeadlinesApp.swift
+//  Top Headlines
+//
+//  Created by Frank Bara on 8/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Top_HeadlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
